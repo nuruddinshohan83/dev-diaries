8b81a7720b2a87e0836e253d369d66e09af50c01
shohan <nuruddin.shohan@yahoo.com>
2024-04-05 03:17:03 +0600

    mantine integrated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { mantine_tw_preset } from "./tailwind.mantine.preset"
 
 const config: Config = {
+  darkMode: ["class", '[data-mantine-color-scheme="dark"]'],
+  presets: [mantine_tw_preset],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
   plugins: [],
-};
-export default config;
+}
+export default config
